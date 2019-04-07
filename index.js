c339c0b40824bb987fbb50f4b277121fb08f5fb3
Jimmy Truong <hoangbao1992@gmail.com>
2019-04-07 14:20:51 -0700

    alpha

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+var app = express();
+var fetch = require('node-fetch');
+const port = process.env.PORT || 8000;
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+  app.get("/rates/:currencyId", function(req, resp){
+    var currencyId = req.params.currencyId;
+    fetch("https://api.exchangeratesapi.io/latest?base="+currencyId+"&symbols=CAD,USD,GBP,EUR,AUD,JPY,CHF,CNY").then(function(resp){
+			
+        return resp.json();
+    }).then(function(json){
+        console.log(json);
+        resp.json(json);
+    });
+
+})
+
+app.get("/history", function(req, resp){
+    
+    fetch("https://api.exchangeratesapi.io/history?start_at=2019-01-01&end_at=2019-01-19&symbols=USD&base=CAD").then(function(resp){
+			
+        return resp.json();
+    }).then(function(json){
+        console.log(json);
+        resp.json(json);
+    });
+
+})
+
+app.listen(port, function(err){
+
+if(err){
+	console.log("Code doesn't work, Jimmy: "+err);
+	return false;
+}
+
+console.log("Port is open for Jimmy!!")
+
+});
