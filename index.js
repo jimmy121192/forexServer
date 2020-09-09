dbb75594e8bd53146b5b92a2031e9410359a5224
Jimmy Truong <hoangbao1992@gmail.com>
2020-09-08 20:10:25 -0700

    update

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ var fetch = require('node-fetch');
 const port = process.env.PORT || 8800;
 
 // const jsonServer = require("json-server");
-// const path = require("path");
-// const server = jsonServer.create();
-// const router = jsonServer.router(path.join(__dirname, "listDB.json"));
-// const middlewares = jsonServer.defaults();
-// server.use(middlewares);
-// server.use(router);
-// server.listen(8080, () => {
-//   console.log("JSON Server is running");
-// });
+    // const path = require("path");
+    // const server = jsonServer.create();
+    // const router = jsonServer.router(path.join(__dirname, "listDB.json"));
+    // const middlewares = jsonServer.defaults();
+    // server.use(middlewares);
+    // server.use(router);
+    // server.listen(8080, () => {
+    //   console.log("JSON Server is running");
+    // });
 
 var http = require("http");
 setInterval(function() {
     http.get("http://jimmy-forexapp.herokuapp.com");
 }, 300000); // every 5 minutes (300000)
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });
 
   app.get("/rates/:currencyId", function(req, resp){
     var currencyId = req.params.currencyId;
